3a5da9f4a91bdfbfd4d4db24a2df6eba21686c66
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-04-12 17:52:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root:$1$PgqmDetr$xqXbP57ysPKh9m36oycQS:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$PgqmDetr$xqXbP57ysPKh9m36oycQS:0:0:99999:7:::/g' etc/shadow
