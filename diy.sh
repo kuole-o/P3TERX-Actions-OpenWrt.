232c1ba75485e5587f1032b48cc53009b8c4242d
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-04-13 22:03:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,6 +11,9 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$PgqmDetr$xqXbP57ysPKh9m36oycQS:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+cd openwrt/package/feeds
+git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable
+
 # 不要直接改源码，换一种方式，下面这些弃用
 # sed -i 's/none/WPA/WPA2/a\set wireless.default_radio${devidx}.key=password/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
