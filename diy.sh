88f208f07ea301decabc995427f87d08e90a1fa8
kuole-o <63436555+kuole-o@users.noreply.github.com>
2020-04-14 10:05:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,6 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 cd openwrt/package/feeds
 git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable
+git clone https://github.com/user1121114685/koolproxyR.git
 
 # 不要直接改源码，换一种方式，下面这些弃用
 # sed -i 's/none/WPA/WPA2/a\set wireless.default_radio${devidx}.key=password/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
